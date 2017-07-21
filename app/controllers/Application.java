601482413c6a42e668ee9cf39fa41d937ed85ff4
Matt Eason <matt.eason@fivium.co.uk>
2017-07-21 17:25:01 +0100

    Add licence list and licence details

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,4 +18,12 @@ public class Application extends Controller {
         return ok(licenceApplication.render(applicationRef, activeTab));
     }
 
+    public Result licenceList(String activeTab) {
+        return ok(licenceList.render(activeTab));
+    }
+
+    public Result licenceDetails(String licenceRef) {
+        return ok(licenceDetails.render(licenceRef));
+    }
+
 }
