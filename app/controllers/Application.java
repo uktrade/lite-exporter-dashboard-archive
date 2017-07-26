1180e0e0d58c1a8a5aa8a4b2732d868e80e896ec
Matt Eason <matt.eason@fivium.co.uk>
2017-07-26 15:30:48 +0100

    Persist selected tab across session for application and licence lists

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,22 +8,35 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() {
-        FormFactory formFactory = new FormFactory(null,null,null);
-        Form form = formFactory.form();
-        return ok(index.render(form));
+  public String setOrRestoreTabState(String tabGroup, String activeTab, String defaultTab) {
+    if(activeTab == null) {
+      String sessionActiveTab = session(tabGroup+"ActiveTab");
+      activeTab = (sessionActiveTab != null) ? sessionActiveTab : defaultTab;
     }
+    session(tabGroup+"ActiveTab", activeTab);
+    return activeTab;
+  }
 
-    public Result licenceApplication(String applicationRef, String activeTab) {
-        return ok(licenceApplication.render(applicationRef, activeTab));
-    }
+  public Result index() {
+    return redirect("/applications");
+  }
 
-    public Result licenceList(String activeTab) {
-        return ok(licenceList.render(activeTab));
-    }
+  public Result applicationList(String activeTab) {
+    String tab = setOrRestoreTabState("applicationList", activeTab, "created-by-you");
+    return ok(applicationList.render(tab));
+  }
 
-    public Result licenceDetails(String licenceRef) {
-        return ok(licenceDetails.render(licenceRef));
-    }
+  public Result licenceApplication(String applicationRef, String activeTab) {
+    return ok(licenceApplication.render(applicationRef, activeTab));
+  }
+
+  public Result licenceList(String activeTab) {
+    String tab = setOrRestoreTabState("licenceList", activeTab, "siels");
+    return ok(licenceList.render(tab));
+  }
+
+  public Result licenceDetails(String licenceRef) {
+  return ok(licenceDetails.render(licenceRef));
+  }
 
 }
