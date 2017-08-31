f8c409b11fb457f67851ab795fa75bc7925bc107
Stefan Brach <stefan.brach@digital.trade.gov.uk>
2017-08-31 14:25:39 +0100

    LITE 731 Status tracker shows reply date for rfi with reply

--- app/components/service/StatusItemViewServiceImpl.java
+++ app/components/service/StatusItemViewServiceImpl.java
@@ -6,9 +6,11 @@ import com.google.common.collect.Multimap;
 import com.google.inject.Inject;
 import components.dao.ApplicationDao;
 import components.dao.RfiDao;
+import components.dao.RfiResponseDao;
 import components.dao.StatusUpdateDao;
 import models.Application;
 import models.Rfi;
+import models.RfiResponse;
 import models.StatusUpdate;
 import models.enums.StatusType;
 import models.view.StatusItemRfiView;
@@ -19,6 +21,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -41,6 +44,7 @@ public class StatusItemViewServiceImpl implements StatusItemViewService {
   private final ProcessingLabelService processingLabelService;
   private final StatusService statusService;
   private final ApplicationDao applicationDao;
+  private final RfiResponseDao rfiResponseDao;
 
   @Inject
   public StatusItemViewServiceImpl(StatusUpdateDao statusUpdateDao,
@@ -50,7 +54,8 @@ public class StatusItemViewServiceImpl implements StatusItemViewService {
                                    StatusExplanationService statusExplanationService,
                                    ProcessingLabelService processingLabelService,
                                    StatusService statusService,
-                                   ApplicationDao applicationDao) {
+                                   ApplicationDao applicationDao,
+                                   RfiResponseDao rfiResponseDao) {
     this.statusUpdateDao = statusUpdateDao;
     this.rfiDao = rfiDao;
     this.timeFormatService = timeFormatService;
@@ -59,6 +64,7 @@ public class StatusItemViewServiceImpl implements StatusItemViewService {
     this.processingLabelService = processingLabelService;
     this.statusService = statusService;
     this.applicationDao = applicationDao;
+    this.rfiResponseDao = rfiResponseDao;
   }
 
   @Override
@@ -94,10 +100,22 @@ public class StatusItemViewServiceImpl implements StatusItemViewService {
 
   private List<StatusItemView> createUpdateStatusItemViews(String appId) {
     List<StatusUpdate> statusUpdates = getStatusUpdates(appId);
-    Multimap<StatusUpdate, Rfi> rfiMultimap = createRfiMultimap(appId, statusUpdates);
+    List<Rfi> rfiList = rfiDao.getRfiList(appId);
+    Multimap<StatusUpdate, Rfi> rfiMultimap = createRfiMultimap(appId, statusUpdates, rfiList);
+    Map<String, RfiResponse> rfiIdToRfiResponseMap = createRfiIdToRfiResponseMap(rfiList);
+
     return statusUpdates.stream()
-        .map(statusUpdate -> getStatusItemView(statusUpdate, rfiMultimap.get(statusUpdate)))
+        .map(statusUpdate -> getStatusItemView(statusUpdate, rfiMultimap.get(statusUpdate), rfiIdToRfiResponseMap))
         .collect(Collectors.toList());
+  }
+
+  private Map<String, RfiResponse> createRfiIdToRfiResponseMap(List<Rfi> rfiList) {
+    List<String> rfiIds = rfiList.stream()
+        .map(Rfi::getRfiId)
+        .collect(Collectors.toList());
+    Map<String, RfiResponse> rfiIdToRfiResponseMap = new HashMap<>();
+    rfiResponseDao.getRfiResponses(rfiIds).forEach(rfiResponse -> rfiIdToRfiResponseMap.put(rfiResponse.getRfiId(), rfiResponse));
+    return rfiIdToRfiResponseMap;
   }
 
   private List<StatusUpdate> getStatusUpdates(String appId) {
@@ -113,26 +131,32 @@ public class StatusItemViewServiceImpl implements StatusItemViewService {
     }).collect(Collectors.toList());
   }
 
-  private StatusItemView getStatusItemView(StatusUpdate statusUpdate, Collection<Rfi> rfiList) {
+  private StatusItemView getStatusItemView(StatusUpdate statusUpdate, Collection<Rfi> rfiList, Map<String, RfiResponse> rfiIdToRfiResponseMap) {
     String status = statusService.getStatus(statusUpdate.getStatusType());
     String statusExplanation = statusExplanationService.getStatusExplanation(statusUpdate.getStatusType());
     String processingLabel = processingLabelService.getProcessingLabel(statusUpdate);
     String processingDescription = processingDescriptionService.getProcessingDescription(statusUpdate);
     List<StatusItemRfiView> statusItemRfiViews = rfiList.stream()
         .sorted(Comparator.comparing(Rfi::getReceivedTimestamp))
-        .map(this::getStatusItemRfiView)
+        .map(rfi -> getStatusItemRfiView(rfi, rfiIdToRfiResponseMap.get(rfi.getRfiId())))
         .collect(Collectors.toList());
     return new StatusItemView(status, statusExplanation, processingLabel, processingDescription, statusItemRfiViews);
   }
 
-  private StatusItemRfiView getStatusItemRfiView(Rfi rfi) {
-    String time = timeFormatService.formatDateAndTime(rfi.getReceivedTimestamp());
-    String description = "Received on " + time;
-    return new StatusItemRfiView(rfi.getAppId(), rfi.getRfiId(), description);
+  private StatusItemRfiView getStatusItemRfiView(Rfi rfi, RfiResponse rfiResponse) {
+    if (rfiResponse == null) {
+      String time = timeFormatService.formatDateAndTime(rfi.getReceivedTimestamp());
+      String description = "Received on " + time;
+      return new StatusItemRfiView(rfi.getAppId(), rfi.getRfiId(), description);
+    } else {
+      String time = timeFormatService.formatDateAndTime(rfiResponse.getSentTimestamp());
+      String description = "Replied to on " + time;
+      return new StatusItemRfiView(rfi.getAppId(), rfi.getRfiId(), description);
+    }
   }
 
-  private Multimap<StatusUpdate, Rfi> createRfiMultimap(String appId, List<StatusUpdate> statusUpdates) {
-    List<Rfi> sortedRfiList = rfiDao.getRfiList(appId).stream()
+  private Multimap<StatusUpdate, Rfi> createRfiMultimap(String appId, List<StatusUpdate> statusUpdates, List<Rfi> rfiList) {
+    List<Rfi> sortedRfiList = rfiList.stream()
         .sorted(Comparator.comparing(Rfi::getReceivedTimestamp))
         .collect(Collectors.toList());
     Multimap<StatusUpdate, Rfi> rfiMap = HashMultimap.create();
