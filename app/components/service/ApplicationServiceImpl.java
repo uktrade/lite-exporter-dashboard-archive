8bbac8bdce9547ae539138643e4ee1a32e80d9ad
Stefan Brach <stefan.brach@digital.trade.gov.uk>
2017-10-03 17:17:16 +0100

    Application cannot be amended if withdrawn or stopped

--- app/components/service/ApplicationServiceImpl.java
+++ app/components/service/ApplicationServiceImpl.java
@@ -1,22 +1,35 @@
 package components.service;
 
 import com.google.inject.Inject;
+import components.dao.NotificationDao;
 import components.dao.StatusUpdateDao;
+import components.dao.WithdrawalApprovalDao;
+import models.NotificationType;
 import models.enums.StatusType;
 
 public class ApplicationServiceImpl implements ApplicationService {
 
   private final StatusUpdateDao statusUpdateDao;
+  private final NotificationDao notificationDao;
+  private final WithdrawalApprovalDao withdrawalApprovalDao;
 
   @Inject
-  public ApplicationServiceImpl(StatusUpdateDao statusUpdateDao) {
+  public ApplicationServiceImpl(StatusUpdateDao statusUpdateDao,
+                                NotificationDao notificationDao,
+                                WithdrawalApprovalDao withdrawalApprovalDao) {
     this.statusUpdateDao = statusUpdateDao;
+    this.notificationDao = notificationDao;
+    this.withdrawalApprovalDao = withdrawalApprovalDao;
   }
 
   @Override
   public boolean isApplicationInProgress(String appId) {
-    return statusUpdateDao.getStatusUpdates(appId).stream()
-        .noneMatch(statusUpdate -> statusUpdate.getStatusType() == StatusType.COMPLETE);
+    boolean isStopped = notificationDao.getNotifications(appId).stream()
+        .anyMatch(notification -> notification.getNotificationType() == NotificationType.STOP);
+    boolean isWithdrawn = withdrawalApprovalDao.getWithdrawalApproval(appId) != null;
+    boolean isComplete = statusUpdateDao.getStatusUpdates(appId).stream()
+        .anyMatch(statusUpdate -> statusUpdate.getStatusType() == StatusType.COMPLETE);
+    return !isStopped && !isWithdrawn && !isComplete;
   }
 
 }
