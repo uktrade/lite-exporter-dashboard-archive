cfc69d3c0d034e70a215faa5c3fd9e64390daf8c
Stefan Brach <brach.stefan@gmail.com>
2018-01-05 17:46:11 +0000

    LITE-928 Made saml location configurable

--- app/components/auth/SamlModule.java
+++ app/components/auth/SamlModule.java
@@ -1,18 +1,18 @@
 package components.auth;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import components.common.auth.SamlUtil;
 import components.common.auth.SpireAuthManager;
-import components.common.auth.SpireSAML2Client;
-import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.context.session.SessionStore;
 import org.pac4j.play.ApplicationLogoutController;
 import org.pac4j.play.PlayWebContext;
 import org.pac4j.play.store.PlayCacheStore;
-import org.pac4j.saml.client.SAML2Client;
-import org.pac4j.saml.client.SAML2ClientConfiguration;
+import play.Application;
 import play.Configuration;
 import play.api.cache.CacheApi;
 
@@ -37,41 +37,15 @@ public class SamlModule extends AbstractModule {
   @Provides
   public Config provideConfig(SessionStore<PlayWebContext> playCacheStore,
                               SamlAuthorizer samlAuthorizer,
-                              SpireAuthManager authManager) {
-
-    //Keystore with a dummy certificate: the outgoing request is not signature checked
-    SAML2ClientConfiguration samlConfig = new SAML2ClientConfiguration("resource:saml/keystore.jks",
-        "keypass",
-        "keypass",
-        configuration.getString("saml.metadataFile"));
-
-    //Maximum permitted age of IdP response in seconds
-    samlConfig.setMaximumAuthenticationLifetime(3600);
-
-    //AKA Saml2:Issuer
-    samlConfig.setServiceProviderEntityId(configuration.getString("saml.issuer"));
-    samlConfig.setServiceProviderMetadataPath("");
-
-    //Custom SAML client which will store response attributes on the Saml Profile
-    SAML2Client saml2Client = new SpireSAML2Client(samlConfig, authManager);
-
-    Clients clients = new Clients(configuration.getString("saml.callbackUrl"), saml2Client);
-
-    clients.setDefaultClient(saml2Client);
-    saml2Client.setCallbackUrl(configuration.getString("saml.callbackUrl"));
-    saml2Client.setIncludeClientNameInCallbackUrl(false);
-
-    Config config = new Config(clients);
-
-    config.setHttpActionAdapter(new SamlHttpActionAdaptor());
-
-    config.setSessionStore(playCacheStore);
-
-    config.addAuthorizer(SamlAuthorizer.AUTHORIZER_NAME, samlAuthorizer);
-
-    return config;
+                              SpireAuthManager authManager,
+                              Provider<Application> application) {
+    return SamlUtil.buildConfig(application,
+        configuration,
+        authManager,
+        playCacheStore,
+        new SamlHttpActionAdaptor(),
+        ImmutableMap.of(SamlAuthorizer.AUTHORIZER_NAME, samlAuthorizer));
   }
-
 
   @Provides
   public SessionStore<PlayWebContext> providePlayCacheStore(CacheApi cacheApi) {
