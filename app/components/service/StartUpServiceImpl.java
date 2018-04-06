667ceafed1aaf241fb7f076f9adb1d5d72682ac6
Stefan Brach <brach.stefan@gmail.com>
2018-04-06 16:46:15 +0100

    LITE-1040 No longer insert test data automatically on startup

--- app/components/service/StartUpServiceImpl.java
+++ app/components/service/StartUpServiceImpl.java
@@ -1,46 +1,15 @@
 package components.service;
 
 import com.google.inject.Inject;
-import components.dao.ApplicationDao;
-import components.service.test.TestDataService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import play.inject.ApplicationLifecycle;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
+/**
+ * By binding this empty implementation on startup, we allow tests to bind their own version of startUpService, for
+ * example to shutdown ehcache instances
+ */
 public class StartUpServiceImpl implements StartUpService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(StartUpServiceImpl.class);
-
-  private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-  private final ApplicationDao applicationDao;
-  private final TestDataService testDataService;
-
   @Inject
-  public StartUpServiceImpl(ApplicationLifecycle lifecycle, ApplicationDao applicationDao, TestDataService testDataService) {
-    this.applicationDao = applicationDao;
-    this.testDataService = testDataService;
-    lifecycle.addStopHook(() -> {
-      executor.shutdown();
-      return CompletableFuture.completedFuture(null);
-    });
-    executor.schedule(this::startUp, 3, TimeUnit.SECONDS);
-  }
-
-  private void startUp() {
-    long applicationCount = applicationDao.getApplicationCount();
-    if (applicationCount == 0) {
-      try {
-        LOGGER.error("Insert start up test data.");
-        testDataService.deleteAllUsersAndInsertStartData();
-      } catch (Throwable throwable) {
-        LOGGER.error("Unable to insert start up test data.", throwable);
-      }
-    }
+  public StartUpServiceImpl() {
   }
 
 }
