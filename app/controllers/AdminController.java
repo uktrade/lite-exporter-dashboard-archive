d1658c5d9950971b8877d4cc3477a473df96e888
Pete Nattress <pete.nattress@fivium.co.uk>
2018-06-28 17:31:09 +0100

    Add ping endpoint for healthchecks

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -1,15 +1,19 @@
 package controllers;
 
+import static play.mvc.Results.ok;
+
 import actions.BasicAuthAction;
 import play.mvc.Result;
-import play.mvc.Results;
 import play.mvc.With;
 
 @With(BasicAuthAction.class)
 public class AdminController {
 
   public Result buildInfo() {
-    return Results.ok(buildinfo.BuildInfo$.MODULE$.toJson()).as("application/json");
+    return ok(buildinfo.BuildInfo$.MODULE$.toJson()).as("application/json");
   }
 
+  public Result ping() {
+   return ok("pong");
+  }
 }
